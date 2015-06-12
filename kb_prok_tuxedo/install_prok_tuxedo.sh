0b4cf32fd6d253c8ca1da4d22347691c2c07f657
Fangfang Xia <levinas@users.noreply.github.com>
2015-06-12 17:44:18 -0500

    Update install_prok_tuxedo.sh

--- kb_prok_tuxedo/install_prok_tuxedo.sh
+++ kb_prok_tuxedo/install_prok_tuxedo.sh
@@ -2,7 +2,11 @@
 
 target=${TARGET-/kb/runtime}
 
-wget --no-check-certificate -P ${target}/bin https://github.com/aswarren/Prok-tuxedo/raw/master/prok_tuxedo.py
+#wget --no-check-certificate -P ${target}/bin https://github.com/aswarren/Prok-tuxedo/raw/master/prok_tuxedo.py
+git clone https://github.com/aswarren/Prok-tuxedo.git
+cp Prok-tuxedo/prok_tuxedo.py ${target}/bin/
+cp Prok-tuxedo/cuffdiff_to_genematrix.py ${target}/lib/
+
 chmod a+x ${target}/bin/prok_tuxedo.py
 
 
