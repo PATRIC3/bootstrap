d08c8bbe650b3794b898c576f9f9ce3d5a549308
Robert Olson <olson@beech.mcs.anl.gov>
2015-06-12 20:32:29 -0500

    remove repo before pull

--- kb_prok_tuxedo/install_prok_tuxedo.sh
+++ kb_prok_tuxedo/install_prok_tuxedo.sh
@@ -3,6 +3,7 @@
 target=${TARGET-/kb/runtime}
 
 #wget --no-check-certificate -P ${target}/bin https://github.com/aswarren/Prok-tuxedo/raw/master/prok_tuxedo.py
+rm -rf Prok-tuxedo
 git clone https://github.com/aswarren/Prok-tuxedo.git
 cp Prok-tuxedo/prok_tuxedo.py ${target}/bin/
 cp Prok-tuxedo/cuffdiff_to_genematrix.py ${target}/bin/
