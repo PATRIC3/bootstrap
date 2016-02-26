233100f2e0dbdfb68589c87ead15b453aa44885c
brettin <brettin@anl.gov>
2016-02-25 18:46:26 -0600

    new installer

---
+++ kb_snpeff/snpeff_build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# set up for snpeff 
+
+target=${TARGET-/usr/local}
+
+if [[ $# -ne 0 ]] ; then
+        target=$1
+        shift
+fi
+
+dst=$target/data
+
+# general steps:
+# download latest
+# unzip latest
+# wrap jars and deploy as scripts?
+# configure location of database
+
+
+wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
+unzip snpEff_latest_core.zip
+
+
+cp -r snpEff $target 
+pushd $target/snpEff
+
+for n in *.jar ; do 
+   p=$(basename $n .jar) ; 
+   echo "\"java\" \"-jar\" \"$target/snpEff/$n\" \"\$@\"" > $target/bin/$p.sh ;
+   chmod a+rx $target/bin/$p.sh ;
+done
+
+popd
