7b722001f95695d0814cad5be81941b66c05a19d
Thomas Brettin <brettin@redwood.mcs.anl.gov>
2016-03-20 12:29:34 -0500

    establish version dependency

--- kb_snpeff/snpeff_build.sh
+++ kb_snpeff/snpeff_build.sh
@@ -18,9 +18,11 @@ dst=$target/data
 # configure location of database
 
 
-wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
-unzip snpEff_latest_core.zip
+# wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
+# unzip snpEff_latest_core.zip
 
+wget https://sourceforge.net/projects/snpeff/files/snpEff_v4_0_core.zip
+unzip snpEff_v4_0_core.zip
 
 cp -r snpEff $target 
 pushd $target/snpEff
